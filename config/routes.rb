b05f11d2353eadb0b5137f5a42375c6cd699a285
Yudistira Nur Sasongko <yudistira.nursasongko@gmail.com>
2018-01-19 11:50:25 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/profile'
+  get 'user/new'
+
+  #static page
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/profile', to: 'static_pages#profile'
+
+  #user
+  get '/signup', to: 'user#new'
+
+  #root
   root 'static_pages#home'
 end
